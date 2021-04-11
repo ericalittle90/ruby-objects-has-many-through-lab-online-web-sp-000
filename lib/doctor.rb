65d6ad5f185093c9c4637d4cf1e755b01468f8d3
Erica Little <eteney90@gmail.com>
2021-04-11 16:26:02 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,13 +1,13 @@
-class Doctor 
+class Doctor
 
-  attr_accessor :name 
+  attr_accessor :name
 
-  @@all = [] 
+  @@all = []
 
   def initialize(name)
-    @name = name 
-    @@all << self 
-  end 
+    @name = name
+    @@all << self
+  end
 
   def self.all
     @@all
