370f4f1a6d4bbb93cf9a494f86f80ee1a9303436
Erica Little <eteney90@gmail.com>
2021-04-11 16:38:14 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,16 @@
+class Appointment 
+
+  attr_accessor :name, :doctor, :patient
+
+  @@all = [] 
+
+  def initialize(date)
+    @date = date 
+    @@all << self 
+  end 
+
+  def self.all 
+    @@all 
+  end 
+
+end 
