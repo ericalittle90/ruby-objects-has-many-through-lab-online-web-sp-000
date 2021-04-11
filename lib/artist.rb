0821292b97f2babaf116ce89867c41a8bb6427e7
Erica Little <eteney90@gmail.com>
2021-04-11 16:13:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,16 @@
-class Artist 
+class Artist
 
-  attr_accessor :name 
+  attr_accessor :name
 
   @@all = []
 
   def initialize(name)
-    @name = name 
+    @name = name
     @@all << self
-  end 
+  end
 
-  def self.all 
-    @@all 
-  end 
+  def self.all
+    @@all
+  end
 
 end 
